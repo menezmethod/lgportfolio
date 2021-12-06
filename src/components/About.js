4a997d9b8a971add6c64c963dc9530002003fa93
Luis Gimenez <72755559+menezmethod@users.noreply.github.com>
2021-12-06 10:03:43 -0700

    about.js had wrong html tag "targer" changed to correct "target" value

--- src/components/About.js
+++ src/components/About.js
@@ -38,7 +38,7 @@ export default function About() {
                             alt="Luis Gimenez, Software Developer"/></p>
                     <AboutMe>
                         Hi there! My name is Luis, and I enjoy creating content for the internet. My interest in web programming began when I was&nbsp;
-                        <a href="https://web.archive.org/web/20010124075000/expage.com/dragonballn" targer="new">10 years old</a>
+                        <a href="https://web.archive.org/web/20010124075000/expage.com/dragonballn" target="new">10 years old</a>
                         &nbsp;and created my first website. Fast forward to the present, and I've had the opportunity to learn new skills beyond HTML and CSS, and I'm now working with cutting-edge technologies. My main focus these days is on developing accessible, inclusive solutions and digital experiences for a diverse clientele. Here are a few technologies with which I've recently worked:
                         <br/>
                         <SkillList>
