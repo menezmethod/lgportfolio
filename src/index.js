f95e5c9694a109d13ccd3bc11a267d277a59864d
Luis Gimenez <72755559+menezmethod@users.noreply.github.com>
2022-01-05 21:32:38 -0500

    removed junk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
 import About from './components/About';
 import MyWork from './components/MyWork';
